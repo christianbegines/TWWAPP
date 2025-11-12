3ea52f989e2d7f3aa6e87d2184c2d5f6db987220
christianbegines <christianbc@ext.zara.com>
2025-11-12 16:06:58 +0100

    put text black

--- app/src/main/java/com/totalwar/warhammer/ui/theme/Theme.kt
+++ app/src/main/java/com/totalwar/warhammer/ui/theme/Theme.kt
@@ -1,45 +1,30 @@
 package com.totalwar.warhammer.ui.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = PrimaryColor,
     secondary = SecondaryColor,
-    onSecondary = ColorOnPrimary
+    onSecondary = ColorOnPrimary,
+    onBackground = PrimaryColor // Ahora los textos por defecto usan primary
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = PrimaryColor,
     secondary = SecondaryColor,
     onSecondary = ColorOnPrimary,
+    onBackground = PrimaryColor // Ahora los textos por defecto usan primary
 )
 
 @Composable
 fun TotalWarhammerAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val context = LocalContext.current
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = DarkColorScheme,
         typography = Typography, // Asegúrate de que Typography está actualizado a M3
         shapes = Shapes,         // Igual para Shapes si los usas
         content = content
